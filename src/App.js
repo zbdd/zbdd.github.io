40e59fc1c960799e16c8a24498a319f23344fc05
Zac Durber <zachary.durber@vgw.co>
2022-08-15 10:56:21 +0800

    fix bug

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
         <img src={bomb} className="App-logo" alt="bomb" />
         <a
           className="App-link"
-          href="https://vgw.co"
+          href="https://vgw.co/careers"
           target="_blank"
           rel="noopener noreferrer"
         >
